d0d3a1a06cadbc160fcf08bb7ea09229faa31d39
gondree <gondree@gmail.com>
2013-06-12 20:45:42 -0700

    Removing old data from being tracked

--- analysis/block-size-plots-ec2.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-(./analysis.py --exp_path ../data/all-times-block-size-ec2/ --Xattrib 'Block size' --Yattrib 'GET' --Xlabel 'Block size (bytes)') &
-(./analysis.py --exp_path ../data/all-times-block-size-ec2/ --Xattrib 'Block size' --Yattrib 'PUT' --Xlabel 'Block size (bytes)') &
-(./analysis.py --exp_path ../data/all-times-block-size-ec2/ --Xattrib 'Block size' --Yattrib 'Create tag' --Xlabel 'Block size (bytes)') &
-(./analysis.py --exp_path ../data/all-times-block-size-ec2/ --Xattrib 'Block size' --Yattrib 'Create challenge' --Xlabel 'Block size (bytes)') &
-(./analysis.py --exp_path ../data/all-times-block-size-ec2/ --Xattrib 'Block size' --Yattrib 'Create proof' --Xlabel 'Block size (bytes)') &
-(./analysis.py --exp_path ../data/all-times-block-size-ec2/ --Xattrib 'Block size' --Yattrib 'Verify file' --Xlabel 'Block size (bytes)') &
--- analysis/cpu-plots.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-(./analysis.py --exp_path ../data/all-times-cpu/ --Xattrib 'File size (kb)' --Yattrib 'Create tag' --Ylabel 'Create tag (sec)') &
-(./analysis.py --exp_path ../data/all-times-cpu/ --Xattrib 'File size (kb)' --Yattrib 'Create challenge' --Ylabel 'Create challenge (sec)') &
-(./analysis.py --exp_path ../data/all-times-cpu/ --Xattrib 'File size (kb)' --Yattrib 'Create proof' --Ylabel 'Create proof (sec)') &
-(./analysis.py --exp_path ../data/all-times-cpu/ --Xattrib 'File size (kb)' --Yattrib 'Verify file' --Ylabel 'Verify file (sec)') &
